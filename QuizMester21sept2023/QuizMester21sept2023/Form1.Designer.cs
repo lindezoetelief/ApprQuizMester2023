0634d6800a4d2de12dd0f1c14d5c3c821f3d1bfe
lindezoetelief <87785@roc-teraa.nl>
2023-09-29 08:54:10 +0200

    Interface for login and register

--- QuizMester21sept2023/QuizMester21sept2023/Form1.Designer.cs
+++ QuizMester21sept2023/QuizMester21sept2023/Form1.Designer.cs
@@ -28,13 +28,123 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.tbxUsername = new System.Windows.Forms.TextBox();
+            this.tbxPassword = new System.Windows.Forms.TextBox();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.lblLoginText = new System.Windows.Forms.Label();
+            this.pcbLoginImage = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbLoginImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.ForeColor = System.Drawing.Color.Ivory;
+            this.btnLogin.Location = new System.Drawing.Point(483, 304);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(154, 39);
+            this.btnLogin.TabIndex = 0;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnLogin.UseVisualStyleBackColor = false;
+            // 
+            // tbxUsername
+            // 
+            this.tbxUsername.BackColor = System.Drawing.Color.Ivory;
+            this.tbxUsername.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.tbxUsername.Location = new System.Drawing.Point(482, 178);
+            this.tbxUsername.Name = "tbxUsername";
+            this.tbxUsername.Size = new System.Drawing.Size(174, 22);
+            this.tbxUsername.TabIndex = 1;
+            // 
+            // tbxPassword
+            // 
+            this.tbxPassword.BackColor = System.Drawing.Color.Ivory;
+            this.tbxPassword.Location = new System.Drawing.Point(482, 255);
+            this.tbxPassword.Name = "tbxPassword";
+            this.tbxPassword.Size = new System.Drawing.Size(174, 22);
+            this.tbxPassword.TabIndex = 2;
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Font = new System.Drawing.Font("Bradley Hand ITC", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsername.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.lblUsername.Location = new System.Drawing.Point(477, 146);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(121, 29);
+            this.lblUsername.TabIndex = 3;
+            this.lblUsername.Text = "Username: ";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Font = new System.Drawing.Font("Bradley Hand ITC", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPassword.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.lblPassword.Location = new System.Drawing.Point(478, 223);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(109, 29);
+            this.lblPassword.TabIndex = 4;
+            this.lblPassword.Text = "Password:";
+            // 
+            // lblLoginText
+            // 
+            this.lblLoginText.AutoSize = true;
+            this.lblLoginText.BackColor = System.Drawing.Color.Ivory;
+            this.lblLoginText.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLoginText.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.lblLoginText.Location = new System.Drawing.Point(474, 67);
+            this.lblLoginText.Name = "lblLoginText";
+            this.lblLoginText.Size = new System.Drawing.Size(137, 54);
+            this.lblLoginText.TabIndex = 5;
+            this.lblLoginText.Text = "Login";
+            // 
+            // pcbLoginImage
+            // 
+            this.pcbLoginImage.BackColor = System.Drawing.Color.Transparent;
+            this.pcbLoginImage.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pcbLoginImage.BackgroundImage")));
+            this.pcbLoginImage.InitialImage = ((System.Drawing.Image)(resources.GetObject("pcbLoginImage.InitialImage")));
+            this.pcbLoginImage.Location = new System.Drawing.Point(-2, -3);
+            this.pcbLoginImage.Name = "pcbLoginImage";
+            this.pcbLoginImage.Size = new System.Drawing.Size(406, 458);
+            this.pcbLoginImage.TabIndex = 6;
+            this.pcbLoginImage.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Ivory;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pcbLoginImage);
+            this.Controls.Add(this.lblLoginText);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblUsername);
+            this.Controls.Add(this.tbxPassword);
+            this.Controls.Add(this.tbxUsername);
+            this.Controls.Add(this.btnLogin);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pcbLoginImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.TextBox tbxUsername;
+        private System.Windows.Forms.TextBox tbxPassword;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Label lblLoginText;
+        private System.Windows.Forms.PictureBox pcbLoginImage;
     }
 }
 
